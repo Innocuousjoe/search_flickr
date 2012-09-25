730f786b443de89096af9cda6db858ab5a45e6f5
Nick Coelius <ncoelius@gmail.com>
2012-09-24 19:37:16 -0700

    moved over js, updated html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SearchFlickr::Application.routes.draw do
   get "photos/index"
-
+  root to: "photos#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
